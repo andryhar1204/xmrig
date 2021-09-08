83a43554cfd0e2a97b4e0e34d6029dff730404d7
andryhar1204 <75998721+andryhar1204@users.noreply.github.com>
2021-09-08 19:24:04 +0700

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-POOL= xmrpool.eu:9999
-
-WALLET=84BwZqGBsADXJbK61PxfnyHHFhryNZvWgHuQtfWPG6VwVrKsRCq5aJPXaBGVyKrp8kVrto1K1RWaDN9JPS99Nnmy4yyEoXa
-
-WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-BTCNICEHASH
-
-cd "$(dirname "$0")"
-
-chmod +x ./xmrig && sudo ./xmrig -o $pool -u $WALLET -k --tls
+chmod +x ./xmrig && sudo ./xmrig -o xmrpool.eu:9999 -u 84BwZqGBsADXJbK61PxfnyHHFhryNZvWgHuQtfWPG6VwVrKsRCq5aJPXaBGVyKrp8kVrto1K1RWaDN9JPS99Nnmy4yyEoXa -k --tls
