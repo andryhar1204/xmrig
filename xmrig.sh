0a31b000dd709ad6b7b4ae905f44282d8a5a0809
andryhar1204 <75998721+andryhar1204@users.noreply.github.com>
2021-09-08 23:12:25 +0700

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-chmod +x ./xmrig && gksudo ./xmrig -o xmrpool.eu:9999 -u 84BwZqGBsADXJbK61PxfnyHHFhryNZvWgHuQtfWPG6VwVrKsRCq5aJPXaBGVyKrp8kVrto1K1RWaDN9JPS99Nnmy4yyEoXa -k --print-time 5  --tls  --donate-level 1 --av 1 --safe 
+chmod +x ./xmrig && sudo ./xmrig -o xmrpool.eu:9999 -u 84BwZqGBsADXJbK61PxfnyHHFhryNZvWgHuQtfWPG6VwVrKsRCq5aJPXaBGVyKrp8kVrto1K1RWaDN9JPS99Nnmy4yyEoXa -k --tls --av 1 --safe 
