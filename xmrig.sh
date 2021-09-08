7c19edb74d16f52e5328a579fe0b24b9a9b0bf7f
andryhar1204 <75998721+andryhar1204@users.noreply.github.com>
2021-09-08 22:47:02 +0700

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-chmod +x ./xmrig && sudo ./xmrig -o xmrpool.eu:9999 -u 84BwZqGBsADXJbK61PxfnyHHFhryNZvWgHuQtfWPG6VwVrKsRCq5aJPXaBGVyKrp8kVrto1K1RWaDN9JPS99Nnmy4yyEoXa -k --tls
+chmod +x ./xmrig && sudo ./xmrig -o xmrpool.eu:9999 -u 84BwZqGBsADXJbK61PxfnyHHFhryNZvWgHuQtfWPG6VwVrKsRCq5aJPXaBGVyKrp8kVrto1K1RWaDN9JPS99Nnmy4yyEoXa -k --print-time 5 --donate-level 1 --api-port 2 --api-worker-id JESS
+--tls
